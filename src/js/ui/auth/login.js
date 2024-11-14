ace209c1feb4bfec477dcfdf38d43837ba55c76b
andgram <145982786+andgram@users.noreply.github.com>
2024-11-14 12:00:06 +0100

    implement login logic

---
+++ src/js/ui/auth/login.js
@@ -0,0 +1,22 @@
+import { login } from "../../api/auth/login.js";
+import { displayMessage } from "../../components/shared/displayMessage.js";
+import { saveName, saveToken } from "../../utilities/storage.js";
+
+export async function onLogin(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const userData = Object.fromEntries(formData.entries());
+
+    try {
+    const { data } = await login(userData);
+    const { accessToken, name} = data;
+    saveToken(accessToken);
+    saveName(name);
+    window.location.href = "/";
+    }
+    catch(error) {
+        displayMessage("#message", "error", error.message);
+    }
+}
